b9f49db853ddc5202ebd998d7af3eea925bc99fd
andrewcargill <andycargill01@aol.com>
2023-06-18 15:20:09 +0200

    create home page and route

---
+++ sawmill/src/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <Button variant="primary">Primary Button</Button>
+        <h1>HOME</h1>
+    </div>
+  );
+};
+
+export default MyComponent;
