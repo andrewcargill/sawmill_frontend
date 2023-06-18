1b09dc65e0de3a93b835ec1ca5d76d0b3c480545
andrewcargill <andycargill01@aol.com>
2023-06-18 20:13:05 +0200

    bootstrap css and js tested and working

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
-      {/* Add your content for the Home page */}
+      <h1>HOME</h1>
+      <Button variant="primary">Test Button</Button>
     </div>
   );
 };
