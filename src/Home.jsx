b8418aa920d8a521336de19981b4506fbc225850
andrewcargill <andycargill01@aol.com>
2023-06-18 22:39:07 +0200

    test link to sawmill-live-api

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import Test from './components/Test';
 
 const Home = () => {
   return (
     <div>
       <h1>HOME</h1>
       <Button variant="primary">Test Button</Button>
+      <Test />
     </div>
   );
 };
---
+++ src/components/Test.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Test = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://sawmill-live-api-ecf54c3f35e6.herokuapp.com/api/lumber/');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h3>Data1: {item.data1}</h3>
+          <p>Data2: {item.data2}</p>
+          <p>Data3: {item.data3}</p>
+        </div>
+      ))}
+      Test Component Content
+    </div>
+  );
+};
+
+export default Test;
